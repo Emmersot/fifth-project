a9fbdd775b89f9e9a10bc2593387902e0a94ffb1
Tyler Emmerson <TylerEmmerson0216@gmaile.com>
2022-06-29 15:47:10 -0700

    test 1 good fail while creating new calc object.

--- __tests__/ageCalculator.tests.js
+++ __tests__/ageCalculator.tests.js
@@ -1,18 +1,14 @@
-import ageCalculator from '../src/js/ageCalculator.js';
+import ageCalculator from './../src/js/ageCalculator.js';
 
 describe('ageCalculator', () => {
-  
-  test('should correctly return a new calc object with the age property', () => {
-    expect(calc.age).toEqual(25)
+  let calc;
+
+  beforeEach(() => {
+    calc = new ageCalculator(25);
   });
-  test('should correctly return a new age in Mercury years)', () => {
-    expect(calc.age).toEqual(104.1)
-//idk what to do
-    return mercuryTime;
-    
+
+  test('should correctly return a new calc object with the age property', () => {
+    expect(calc.age).toEqual(25);
+  
   });
 });
-//set global age based on inputted value or my own this.age = 25
-//create mercuryAge
-//let ageOnMercury = (earthAge/0.24)
-//
